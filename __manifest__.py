05c388a658ee18b7be6e4b3fb4722481527ed5ae
Santiago Bacale Ortiz <santiagobacale@gmail.com>
2021-03-18 14:03:24 +0100

    Mi primer commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*- 
+
+{
+    'name': 'Servicio',
+    'summary': """Este módulo nos permitirá gestionar la asignación de servicios""",
+    'version': '13.0.0.0.0',
+    'description': """Este módulo nos permitirá gestionar la asignación de servicios""",
+    'author': 'Santiago Bacale Ortiz',
+    'company': 'practicasGuadaltech',
+    'website': ' ',
+    'category': 'Tools',
+    'depends': ['base'],
+    'license': 'AGPL-3',
+    'data': [
+        'views/view.xml', 'security/ir.model.access.csv',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
